ca838f8207fc448fc3dde22ee216cf81a1d1f501
Alvin-Karanja <alvin2@protonmail.com>
2020-07-15 14:28:43 +0300

    Generate constructor for functions

--- src/sudoku/problemdomain/Coordinates.java
+++ src/sudoku/problemdomain/Coordinates.java
@@ -1,4 +1,13 @@
 package sudoku.problemdomain;
 
 public class Coordinates {
+    // Functions
+    private final int x;
+    private final int y;
+
+    // Constructor
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
