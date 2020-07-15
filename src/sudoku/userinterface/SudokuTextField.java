4c4914843b745f424eb919b4b72c34bf0fd97532
Alvin-Karanja <alvin2@protonmail.com>
2020-07-15 16:15:28 +0300

    Generated constructors and getters

--- src/sudoku/userinterface/SudokuTextField.java
+++ src/sudoku/userinterface/SudokuTextField.java
@@ -1,4 +1,23 @@
 package sudoku.userinterface;
 
-public class SudokuTextField {
+import javafx.scene.control.TextField;
+
+public class SudokuTextField extends TextField {
+    private final int x;
+    private final int y;
+
+    // Constructor
+    public SudokuTextField(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getters
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
