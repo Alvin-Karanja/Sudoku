b824047f37943f910cbe021f135ebbb29fd1645f
Alvin-Karanja <alvin2@protonmail.com>
2020-07-15 14:21:37 +0300

    Generated constructors and getters

--- src/sudoku/problemdomain/SudokuGame.java
+++ src/sudoku/problemdomain/SudokuGame.java
@@ -1,4 +1,24 @@
 package sudoku.problemdomain;
 
 public class SudokuGame {
+    private final GameState gameState;
+    private final int[][] gridState;
+
+    // Constant
+    public static final GRID_BOUNDARY = 9;
+
+    // Constructors
+    public SudokuGame(GameState gameState, int[][] gridState) {
+        this.gameState = gameState;
+        this.gridState = gridState;
+    }
+
+    // Getters
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public int[][] getGridState() {
+        return gridState;
+    }
 }
