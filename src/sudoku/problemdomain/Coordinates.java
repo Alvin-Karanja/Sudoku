5a117321863c5a1a883c5447717d85a096a84aa2
Alvin-Karanja <alvin2@protonmail.com>
2020-07-15 15:27:48 +0300

    implement hashcode function

--- src/sudoku/problemdomain/Coordinates.java
+++ src/sudoku/problemdomain/Coordinates.java
@@ -1,5 +1,9 @@
 package sudoku.problemdomain;
 
+import java.util.Objects;
+
+import static com.sun.tools.javac.main.Option.O;
+
 public class Coordinates {
     // Functions
     private final int x;
@@ -23,5 +27,15 @@ public class Coordinates {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return x == that.x &&
+                y == that.y;
+    }
+
+    // Hash code function
+    @Override
+    public int hashCode(){
+        return Objects.hash(x, y);
     }
 }
